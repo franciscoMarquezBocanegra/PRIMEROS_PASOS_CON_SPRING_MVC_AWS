4ba79e540dc495cb46cea221f1d75e000fdae8e6
franciscoMarquezBocanegra <francisco.marquez@mail.escuelaing.edu.co>
2023-06-28 23:20:39 -0500

    entrega final

---
+++ calculadoramvc/src/main/java/com/mycompany/calculadoramvc/Calculadoramvc.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoramvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author francisco.marquez
+ */
+
+@SpringBootApplication
+public class Calculadoramvc {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculadoramvc.class, args);
+    }
+}
